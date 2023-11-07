57386f1ab8a23f6529ef89063d9a1e87e9630012
arijitnus <66009368+arijitnus@users.noreply.github.com>
2023-11-07 13:55:22 +0800

    Update Sulfur_gene_counts_boxplot_spheres.R

--- Supplementaries/Sulfur_gene_counts_boxplot_spheres.R
+++ Supplementaries/Sulfur_gene_counts_boxplot_spheres.R
@@ -8,46 +8,42 @@ names(df)
 df$sphere<-as.factor(df$sphere)
 level_order<-c("Soil","Rhizosphere","Phyllosphere")
 cols=c("#4C9900","#CC6600",'#663300')
+
 #boxplot of total number of BGCs per genome from isolates and MAGs
-box<-ggplot(df,aes(x=factor(sphere,levels = level_order),y=unique_count))+
+box<-ggplot(df,aes(x=factor(sphere,levels = level_order),y=`count/Mb`))+
   geom_boxplot(aes(col=sphere)) +
   geom_jitter(aes(col=sphere), size=0.2, alpha=0.2) +
   scale_color_manual(values = cols)+
   scale_fill_manual(values = cols)+
   theme_classic() +
   xlab("")+
-  ylab("Number of sulfur metabolic genes per genome")+
+  ylab("Number of unique sulfur metabolic genes per Mb genome")+
   theme(axis.text.x = element_text(size = 14),
         axis.text.y = element_text(size = 14),
         axis.title.y = element_text(size = 14),
         axis.title.x=element_text(size=14),
         axis.line = element_line(colour="black", size = 0.7),
         strip.background = element_blank())+
-  scale_y_continuous(limits = c(0, 120), expand = expansion(mult = c(0, 0)))
+  scale_y_continuous(limits = c(0, 40), expand = expansion(mult = c(0, 0)))
 box
 
-shapiro.test(df$unique_count)
+shapiro.test(df$`count/Mb`)
 #Not normal. So fit a wilcox test for the differences
 library(dunn.test)
-kruskal.test(unique_count~sphere,data =df)
-stat_sphere<-dunn.test(df$unique_count,df$sphere,wrap = TRUE, method = "bh")
-head(stat_rescue_biomass$P.adjusted)
+kruskal.test(`count/Mb`~sphere,data =df)
+stat_sphere<-dunn.test(df$`count/Mb`,df$sphere,wrap = TRUE, method = "bh")
+stat_sphere$comparisons
+stat_sphere$P.adjusted
+library(dplyr)
+#mean count per Mb genome is :
+df%>%group_by(sphere)%>%summarise(avg=mean(`count/Mb`))
+# A tibble: 3 × 2
+#sphere         avg
+#<fct>        <dbl>
+ # 1 Phyllosphere  12.9
+#2 Rhizosphere   12.3
+#3 Soil          10.8
 
-#Result Rhizo vs Phyllo <0.0001, Rhizos vs soil <0 Soil vs phyllo =0.12
-#Col Mean-|
-  #Row Mean |   Phyllosp   Rhizosph
 
-ggsave(
-  "Sulfur_gene_count_spheres.tiff",
-  plot = last_plot(),
-  device = NULL,
-  path = NULL,
-  scale = 1,
-  width = 6,
-  height = 6,
-  units = "in",
-  dpi = 400,
-)
-dev.off()
 
 
